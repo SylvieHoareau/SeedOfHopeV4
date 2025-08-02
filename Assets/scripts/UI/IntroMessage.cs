f303263131ef870fd5ee71a87fd2f61b963f8129
hoareausylvie <hoareauylvie@outlook.fr>
2025-08-02 12:27:48 +0400

    Modification de l'UI

--- Assets/scripts/UI/IntroMessage.cs
+++ Assets/scripts/UI/IntroMessage.cs
@@ -8,8 +8,8 @@ public class IntroMessage : MonoBehaviour
 
     public string message =
         "[IA LOG]" +
-        "Collectez de l'eau, des graines et de l'engrais pour restaurer la terre.\n" +
-        "Interragisez avec moi (E) lorsque vous êtes prêt. \n" +
+        "Collecte de l'eau, des graines et de l'engrais pour restaurer la terre.\n" +
+        "Interragis avec moi (E) lorsque tu es prêt. \n" +
         "Utilise les flèches du clavier pour te déplacer";
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
